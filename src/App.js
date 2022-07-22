cd21eef0353b48a57622ea08251d82335d670a80
DESKTOP-S5M2HJU\Jo <whrudwls08@naver.com>
2022-07-22 14:42:03 +0900

    불필요한 폴더 삭제

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
